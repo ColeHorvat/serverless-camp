e0e3c5e23f9aa50636e63f21e5f38b6a55c47c21
Cole Horvat <colehorvat@pop-os.localdomain>
2022-06-14 23:09:13 -0700

    Week 1 Step 5, edited hackervoice function to add password authentication

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,8 +2,11 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const password = req.query.password || (req.body && req.body.password);
+    const outputMessage = ((password == "letmein") 
+    ? "Access granted"
+    : "Access denied"); 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: outputMessage
     };
 }
