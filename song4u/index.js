972627be25ca60ac53f6c2aa3f106508b6eefb82
Cole Horvat <colehorvat@pop-os.localdomain>
2022-06-27 00:29:20 -0700

    I figured it out

--- song4u/index.js
+++ song4u/index.js
@@ -1,7 +1,7 @@
-const QueryString = require('qs')
+const querystring = require('qs')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    const queryObject = QueryString.parse(req.body);
+    const queryObject = querystring.parse(req.body);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
