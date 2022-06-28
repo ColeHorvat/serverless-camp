8bf08f4720921e21e7083abdff0152c075033c8f
Cole Horvat <colehorvat@pop-os.localdomain>
2022-06-27 22:00:57 -0700

    Week 2 Step 3

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -5,33 +5,46 @@ const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    const boundary = multipart.getBoundary(req.headers['content-type'])
-    const body = req.body;
-    const parts = multipart.Parse(body, boundary)
-    
-    let fileType = parts[0].type
-    let ext = ""
-    
-    if(fileType === 'image/png')
-        ext = "png"
-    else if(fileType === 'image/jpeg')
-        ext = "jpeg"
-    else if(fileType === 'image/jpg')
-        ext = "jpg"
+        
 
-    let responseMessage = await uploadFile(parts, ext)
+    let responseMessage = ""
+    try {
+        //Initialize codename
+        let codename = req.headers['codename']
+        
+        // Parse body
+        const boundary = multipart.getBoundary(req.headers['content-type'])
+        const body = req.body;
+        const parts = multipart.Parse(body, boundary)
+
+        //Determine file type
+        let fileType = parts[0].type
+        let ext = ""
+        
+        if(fileType === 'image/png')
+            ext = "png"
+        else if(fileType === 'image/jpeg')
+            ext = "jpeg"
+        else if(fileType === 'image/jpg')
+            ext = "jpg"
+
+        responseMessage = await uploadFile(parts, ext, codename)
+    } catch(err) {
+        context.log("Undefined image body")
+        responseMessage = "Sorry! No image attached";
+    }
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
 }
 
-async function uploadFile(parsedBody, ext) {
+async function uploadFile(parsedBody, ext, fileName) {
     const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString)
     const containerName = "images"
     const containerClient = blobServiceClient.getContainerClient(containerName)
 
-    const blobName = "test." + ext;
+    const blobName = fileName + "." + ext;
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
     const resp = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
