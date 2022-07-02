0ee0cecbcb86c6b31023b75c611cd1e1f4165741
Cole Horvat <colehorvat@pop-os.localdomain>
2022-07-02 02:34:46 -0700

    Finished Week 3 Step 5

--- bunnimage-timer/index.js
+++ bunnimage-timer/index.js
@@ -1,17 +1,18 @@
 const { BlobServiceClient } = require('@azure/storage-blob')
-const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING
+//const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING
+const connectionString = "DefaultEndpointsProtocol=https;AccountName=colecampstorage;AccountKey=TzR2HvWg3ems0OUt6LpO9KxyO4edPUGk+Z0OO5avkBSWTRGC45/46ITItFoEfFqH7dm2mygRkGcA+AStJqP6vg==;EndpointSuffix=core.windows.net"
 const account = "colecampstorage"
 
 module.exports = async function (context, myTimer) {
 
-    context.log('--- Starting Deletion ---');
+    context.log("--- Starting Deletion ---");
+    context.log(connectionString)
     const blobServiceClient = await BlobServiceClient.fromConnectionString(connectionString)
     const deleteContainer = "images"
     const blobContainerClient = await blobServiceClient.getContainerClient(deleteContainer)
 
     for await (const blob of blobContainerClient.listBlobsFlat()) {
-        context.log('Deleting blob: ' + blob.name)
         await blobContainerClient.deleteBlob(blob.name)
     }
-    context.log('Just deleted all your blobs!', timeStamp);   
+    context.log("Just deleted all your blobs!");   
 };
