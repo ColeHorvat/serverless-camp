acea37461b6faa865b6e033d65ac010d6bf5f0f1
Cole Horvat <colehorvat@pop-os.localdomain>
2022-07-09 19:22:11 -0600

    Week 4 Step 3

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 const bunnForm = document.getElementById("bunnForm");
 const imageInput = document.getElementById("image")
 
-bunnForm.addEventListener('submit', function(event) {
+
+bunnForm.addEventListener('submit', async function(event) {
 	event.preventDefault();
+
 	const username = document.getElementById('username').value
 	const image = document.getElementById('image').value
 	const output = document.getElementById('output')
 
-	
+	const payload = new FormData();
+	const file = imageInput.files[0]; // fileInput is the file upload input element
+	await payload.append("file", file);
+	console.log(file)
+	console.log(payload.entries());
+
 	if(image == '')
 		alert("No image error...")
 	else if(username == null || username == '')
 		alert("No name error...")
-	else
-		output.textContent = username + "❤"
+
+	let options = {
+		method: "POST",
+		body: payload,
+		headers: {
+			'codename': username
+		}
+
+	}
+	let resource = "https://serverless-camp-2022.azurewebsites.net/api/bunnimage-upload?code=uj5HYqz1r_HZPsOPXVUnU5GAHB08k7gMW01gLPqqcLmvAzFujYIzMA=="
+	/* let resource = "http://localhost:7071/api/bunnimage-upload" */
+	try {
+		const response = await fetch(resource, options)
+		const data = await response.text()
+		console.log(data)
+		output.textContent = "Your image has been stored successfully!"
+	} catch (e) {
+		console.log(e)
+	}
+	
 })
 
 imageInput.addEventListener('change', function(event) {
