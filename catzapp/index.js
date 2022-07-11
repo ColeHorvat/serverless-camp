357fb9b3eab4bab79cb109269172a378fd87353d
Cole Horvat <colehorvat@pop-os.localdomain>
2022-07-11 01:18:35 -0600

    Week 4 Step 6

--- catzapp/index.js
+++ catzapp/index.js
@@ -3,8 +3,20 @@ let catInput = document.getElementById("catInput")
 let catImage = document.getElementById("image")
 
 catButton.addEventListener("click", async function (e) {
-	const baseUrl = "https://cataas.com/cat/says/"
-	let text = catInput.value
+	const name1 = document.getElementById("name1").value
+	const name2 = document.getElementById("name2").value
+	const name3 = document.getElementById("name3").value
+	const name4 = document.getElementById("name4").value
+	const baseUrl = "https://serverless-camp-2022.azurewebsites.net/api/twocatz?code=I0K-pA5q6IWoEqgo3RiB9i2kW9Iv_fU62SM8TH7_xevyAzFuypJ53w=="
+
+	let params = new URLSearchParams({
+        'name1': name1,
+        'name2': name2,
+		'name3': name3,
+		'name4': name4,
+
+    })
+
 
 	catImage.src = baseUrl + text
 
